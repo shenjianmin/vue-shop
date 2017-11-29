b2136165eb24d1e5e97da8d046da13f88149d02c
shenjianmin <alimincode@163.com>
2017-11-29 14:07:41 +0800

    vue商城

---
+++ client/src/components/NavBread.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="bread">
+    <div class="bread-wrap">
+      <nav>
+        <a href="/"> 首页 </a>
+        <slot></slot>
+      </nav>
+    </div>
+  </section>
+</template>
+<script>
+import '../assets/css/bread.css'
+</script>
